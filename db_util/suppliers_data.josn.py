63addc23ada5e5297fd4a8ef6932c9e3144e4212
Forbilly <739146106@qq.com>
2019-12-13 16:17:50 +0800

    sss

---
+++ db_util/suppliers_data.josn.py
@@ -0,0 +1,369 @@
+[{
+    "ModelName" : "CRG0603F15K",
+    "Tiered" : [ 
+        [ 
+            1, 
+            0.1
+        ], 
+        [ 
+            10, 
+            0.032
+        ], 
+        [ 
+            100, 
+            0.013
+        ], 
+        [ 
+            1000, 
+            0.00584
+        ], 
+        [ 
+            2500, 
+            0.00507
+        ]
+    ],
+    "BrandName" : "TE Connectivity Passive Product",
+    "increment" : 1,
+    "Stock" : [ 
+        2507, 
+        1
+    ],
+    "change_count" : 0,
+    "url" : "https://www.digikey.cn/product-detail/en/te-connectivity-passive-product/CRG0603F15K/A121558CT-ND/5246508",
+    "GoodsSn" : "5246508",
+    "DocUrl" : "https://www.te.com/commerce/DocumentDelivery/DDEController?Action=srchrtrv&DocNm=1773204&DocType=DS&DocLang=English",
+    "OtherModelName" : "A121558CT-ND",
+    "Desc" : "RES SMD 15K OHM 1% 1/10W 0603"
+},
+{
+    "ModelName" : "LM358ADR",
+    "url" : "https://www.digikey.cn/product-detail/en/texas-instruments/LM358ADR/296-14601-6-ND/747807",
+    "GoodsSn" : "747807",
+    "Tiered" : [ 
+        [ 
+            1, 
+            0.36
+        ], 
+        [ 
+            10, 
+            0.295
+        ], 
+        [ 
+            100, 
+            0.2011
+        ], 
+        [ 
+            500, 
+            0.1508
+        ], 
+        [ 
+            1000, 
+            0.11309
+        ]
+    ],
+    "BrandName" : "TI",
+    "increment" : 1,
+    "Stock" : [ 
+        123061, 
+        1
+    ],
+    "DocUrl" : "http://www.ti.com/general/docs/suppproductinfo.tsp?distId=10&gotoUrl=http%3A%2F%2Fwww.ti.com%2Flit%2Fgpn%2Flm358",
+    "OtherModelName" : "296-14601-6-ND",
+    "Desc" : "IC OPAMP GP 2 CIRCUIT 8SOIC",
+    "change_count" : 3
+},
+{
+    "ModelName" : "ECS-40-20-5PX-TR",
+    "url" : "",
+    "GoodsSn" : "827702",
+    "Tiered" : [ 
+        [ 
+            1, 
+            0.26
+        ], 
+        [ 
+            10, 
+            0.218
+        ], 
+        [ 
+            50, 
+            0.1962
+        ], 
+        [ 
+            100, 
+            0.1745
+        ], 
+        [ 
+            500, 
+            0.16574
+        ]
+    ],
+    "BrandName" : "ECS Inc.",
+    "increment" : 1,
+    "Stock" : [ 
+        16154, 
+        1
+    ],
+    "DocUrl" : "https://www.ecsxtal.com/store/pdf/csm-7x.pdf",
+    "OtherModelName" : "XC1238CT-ND",
+    "Desc" : "4MHz ±30ppm 晶体 20pF  HC-49/US",
+    "change_count" : 3
+},
+{
+    "ModelName" : "SJ2-35954D-SMT-TR",
+    "time" : 1573523853,
+    "Tiered" : [ 
+        [ 
+            1, 
+            1.33
+        ], 
+        [ 
+            10, 
+            1.17
+        ], 
+        [ 
+            25, 
+            1.1
+        ], 
+        [ 
+            50, 
+            1.05
+        ], 
+        [ 
+            100, 
+            1.01
+        ], 
+        [ 
+            250, 
+            0.912
+        ]
+
+    ],
+    "BrandName" : "CUI Inc.",
+    "Stock" : [ 
+        1689, 
+        1
+    ],
+    "url" : "",
+    "GoodsSn" : "490-SJ2-35954DSMT-TR",
+    "DocUrl" : "https://www.mouser.com/datasheet/2/670/sj2-3595x-smt-1313292.pdf",
+    "Desc" : "电话连接器 3.5mm Audio Jack SMT 4cond Tip/Ring swtch",
+    "errnum" : 0,
+    "increment" : 1,
+    "package" : "",
+    "change_count" : 1,
+    "OtherModelName" : "490-SJ2-35954DSMT-TR"
+},
+{
+    "ModelName" : "LM358EDR2G",
+    "Tiered" : [ 
+        [ 
+            1, 
+            0.37
+        ], 
+        [ 
+            10, 
+            0.256
+        ], 
+        [ 
+            100, 
+            0.118
+        ], 
+        [ 
+            1000, 
+            0.091
+        ], 
+        [ 
+            2500, 
+            0.077
+        ], 
+        [ 
+            10000, 
+            0.07
+        ], 
+        [ 
+            25000, 
+            0.066
+        ], 
+        [ 
+            50000, 
+            0.059
+        ], 
+        [ 
+            100000, 
+            0.057
+        ]
+    ],
+    "BrandName" : "ON Semiconductor",
+    "Stock" : [ 
+        1899, 
+        1
+    ],
+    "url" : "",
+    "GoodsSn" : "863-LM358EDR2G",
+    "DocUrl" : "https://www.mouser.com/datasheet/2/308/LM358-D-106278.pdf",
+    "Desc" : "运算放大器 - 运放 OP AMP LOW PWR DUAL",
+    "increment" : 1,
+    "package" : "2500",
+    "OtherModelName" : "863-LM358EDR2G",
+    "change_count" : 2
+},
+{
+    "ModelName" : "LM358ADGKR",
+    "url" : "",
+    "PN" : "mouser",
+    "GoodsSn" : "595-LM358ADGKR",
+    "Tiered" : [ 
+        [ 
+            1, 
+            0.36
+        ], 
+        [ 
+            10, 
+            0.271
+        ], 
+        [ 
+            100, 
+            0.147
+        ], 
+        [ 
+            1000, 
+            0.11
+        ], 
+        [ 
+            2500, 
+            0.095
+        ], 
+        [ 
+            10000, 
+            0.089
+        ], 
+        [ 
+            22500, 
+            0.082
+        ], 
+        [ 
+            50000, 
+            0.079
+        ], 
+        [ 
+            100000, 
+            0.076
+        ]
+    ],
+    "BrandName" : "TI",
+    "DT" : [ 
+        "香港：4-7", 
+        "大陆：5-9"
+    ],
+    "Stock" : [ 
+        17629, 
+        1
+    ],
+    "DocUrl" : "http://www.ti.com/general/docs/suppproductinfo.tsp?distId=26&gotoUrl=http%3A%2F%2Fwww.ti.com%2Flit%2Fgpn%2Flm358a",
+    "Desc" : "运算放大器 - 运放 Dual Operational Amplifier",
+    "increment" : 1,
+    "PriceType" : 1,
+    "package" : "Reel",
+    "change_count" : 2,
+    "OtherModelName" : "595-LM358ADGKR"
+},
+{
+    "ModelName" : "LF33ABDT-TR",
+    "Tiered" : [ 
+        [ 
+            2500, 
+            0.37
+        ], 
+        [ 
+            5000, 
+            0.3
+        ], 
+        [ 
+            7500, 
+            0.295
+        ]
+    ],
+    "BrandName" : "STM",
+    "Stock" : [ 
+        0, 
+        5000
+    ],
+    "url" : "https://www.futureelectronics.cn/en/p/1400506",
+    "GoodsSn" : "LF33ABDT-TR$$STMicroelectronics$$A_1",
+    "DocUrl" : "http://www.st.com/web/en/resource/technical/document/datasheet/CD00000546.pdf",
+    "Desc" : "Features and Applications     The LF33ABDT-TR is a very low drop regulators available in PENTAWATT, TO-220, TO-220FP, DPAK and PPAK package and in a wide range of output voltages. The very low drop voltage (0.45 V) and the very low quiescent current make them particularly suitable for low noise, low power applications and specially in battery powered systems. In the 5 pins configuration (PENTAWATT and PPAK) a shutdown logic control function is available (pin 2, TTL compatible). This means that when the device is used as a local regulator, it is possible to put a part of the board in standby, decreasing the total power consumption. In the three terminal configuration the device has the same electrical performance, but is fixed in the ON state. It requires only a 2.2 μF capacitor for stability allowing space and cost saving.Features: Very low dropout voltage (0.45 V)  Very low quiescent current (typ. 50 μA in OFF mode, 500 μA in ON mode)  Output current up to 500 mA  Logic-controlled electronic shutdown  Output voltages of 1.5; 1.8; 2.5; 3.3; 4.7; 5; 6; 8; 8.5; 9; 12 V  Automotive Grade product: 1.8 V, 2.5 V, 3.3 V, 5.0 V, 8.0 V, 8.5 V VOUT in DPAK and PPAK packages  Internal current and thermal limit  Only 2.2 μF for stability  Available in ± 1% (AB) or ± 2% (C) selection at 25 °C  Supply voltage rejection: 80 db (typ.)  Temperature range: -40 to 125 °C",
+    "increment" : 2500,
+    "change_count" : 3
+},
+{
+    "ModelName" : "ADM232AARN",
+    "Tiered" : [ 
+        [ 
+            1, 
+            2.69
+        ], 
+        [ 
+            10, 
+            1.86
+        ], 
+        [ 
+            50, 
+            1.68
+        ]
+    ],
+    "BrandName" : "Analog Devices",
+    "Stock" : [ 
+        81, 
+        1
+    ],
+    "url" : "",
+    "GoodsSn" : "AD01-0104349",
+    "DocUrl" : "http://www.analog.com/media/en/technical-documentation/data-sheets/ADM222_232A_242.pdf",
+    "Desc" : ""
+},
+{
+    "ModelName" : "LM358ADT",
+    "Tiered" : [ 
+        [ 
+            1, 
+            3.51
+        ], 
+        [ 
+            10, 
+            2.72
+        ], 
+        [ 
+            100, 
+            1.48
+        ], 
+        [ 
+            1000, 
+            1.11
+        ], 
+        [ 
+            2500, 
+            0.951
+        ], 
+        [ 
+            10000, 
+            0.891
+        ], 
+        [ 
+            25000, 
+            0.822
+        ]
+    ],
+    "BrandName" : "STM",
+    "Stock" : [ 
+        0, 
+        1
+    ],
+    "url" : "http://hk.element14.com/stmicroelectronics/lm358adt/operationalamplifier/dp/2382610?st=LM358ADT",
+    "GoodsSn" : "2382610",
+    "DocUrl" : "http://www.farnell.com/datasheets/2000357.pdf",
+    "Desc" : "STMICROELECTRONICS - LM358ADT - Operational Amplifier, Dual, 2 Amplifier, 1.1 MHz, 0.6 V/µs, 3V to 30V, SOIC, 8 Pins",
+    "increment" : 1,
+    "change_count" : 1
+}
+]
